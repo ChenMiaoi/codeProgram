b0e03eae9b76ce91855914cd037c099f757c55a7
chenmiaoi <3026066798@qq.com>
2022-12-07 08:17:21 +0800

    clean the work tree

--- cpp/CppStd11/Atour/main.cc
+++ cpp/CppStd11/Atour/main.cc
@@ -1,3 +1,12 @@
-//
-// Created by Chenmiaoi on 2022/12/4.
-//
+#include <iostream>
+#include <set>
+
+int main() {
+    double x = 3.1415'9265'4646;
+    double x1 = 0x7546'ddde'45;
+    double x2 = 04657'123;
+    double x3 = 0b0101'1100'1101;
+    std::set<int> st;
+    auto a = st.begin();
+    return 0;
+}
