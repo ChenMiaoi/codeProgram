7fc69232ec7704e85c0922517d54aa81416de855
chenmiaoi <3026066798@qq.com>
2022-12-31 22:08:48 +0800

    The essential operation done

--- cpp/CppStd11/Atour/EssentialOperations.cc
+++ cpp/CppStd11/Atour/EssentialOperations.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <complex>
 #include "Operation-Vector//Vector.h"
 
 template <typename Sometype>
@@ -45,7 +47,41 @@ X<int> f() {
     return z;
 }
 
+class Matrix {
+public:
+    Matrix& operator= (const Matrix& matrix); // assign m to *this; return a reference
+    // Matrix operator+ (const Matrix& matrix);
+};
+
+Matrix operator+ (const Matrix& matrix1, const Matrix& matrix2);
+
+//class R {
+//    auto operator<=> (const R& a) const = default;
+//};
+//
+//void user(const R& r1, const R& r2) {
+//    bool b1 = (r1 <=> r2) == 0; // r1 == r2;
+//    bool b2 = (r1 <=> r2) < 0; // r1 < r2;
+//    bool b3 = (r1 <=> r2) > 0; // r1 > r2;
+//
+//    bool b4 = (r1 == r2);
+//    bool b5 = (r1 < r2);
+//}
+
+constexpr std::complex<long double> operator""_i (long double arg) {
+    return {0, arg};
+}
+
+constexpr std::chrono::duration<int> operator""_h (unsigned long long hour) {
+    return {};
+}
+
 int main() {
     X<int> x = f();
+    std::string str = "hello";
+    using namespace std::literals::string_literals;
+    std::string str_s = "hello"s;
+    using namespace std::literals::chrono_literals;
+    std::chrono::duration time = 12h-5min-3s;
     return 0;
 }
