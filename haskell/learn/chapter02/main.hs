76a240150b69b0c0478c5c8780a7715825db644e
chenmiaoi <3026066798@qq.com>
2023-08-12 16:22:05 +0800

    read over the introduce

--- haskell/learn/chapter02/main.hs
+++ haskell/learn/chapter02/main.hs
@@ -18,3 +18,50 @@ f1 (x, y) = 4 * x + 5 * y + 1
 -- curring
 f' :: Num a => a -> a -> a
 f' x y = 4 * x + 5 * y + 1
+
+f1' :: Num a => a -> a -> a
+-- f1' = \x -> \y -> 4 * x + 5 * y + 1
+f1' = \x y -> 4 * x + 5 * y + 1
+
+g :: Integer -> Integer -> Integer
+-- g = \y -> \x -> (+) x y
+g = (+)
+
+s :: Double -> Double -> Double -> Double
+s a b c = let p = (a + b + c) / 2 in sqrt (p * (p - a) * (p - b) * (p - c))
+
+s' :: Double -> Double -> Double -> Double
+s' a b c = sqrt (p * (p - a) * (p - b) * (p - c))
+            where
+                p = (a + b + c) / 2
+
+isTwo :: Int -> Bool
+isTwo n = if n == 2 then True else False
+
+month :: Int -> Int
+month n = case n of 
+    1 -> 31
+    2 -> 28
+    9 -> 30
+    _ -> error "invalid month"
+
+abs' :: (Num a, Ord a) => a -> a
+abs' n
+    | n > 0 = n
+    | otherwise = -n
+
+fib :: (Ord t, Num t, Num a) => t -> a
+fib n 
+    | n < 0 = 0
+    | n == 1 = 1
+    | otherwise = fib (n - 1) + fib (n - 2)
+
+head' :: [a] -> a
+head' [] = error "empty list"
+head' (x:_) = x
+
+infixr 5 <->, <+>
+
+(<->), (<+>) :: Int -> Int -> Int
+(<->) x y = x - y
+(<+>) x y = x + y
